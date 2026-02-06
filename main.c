a463307476b6aa2093fc30e32a2d8f3cf5149b28
Andria Shengelia <shengeliaandria9@gmail.com>
2026-02-06 21:26:01 +0400

    Open terminal in raw mode and disable it on exit

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
+#include <termios.h>
 #include <unistd.h>
+#include <termio.h>
+#include <stdlib.h>
+
+
+
+struct termios orig_terminos;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_terminos);
+}
+
+void enableRawMode(){
+    tcgetattr(STDIN_FILENO,&orig_terminos);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_terminos;
+
+    tcgetattr(STDERR_FILENO,&raw);
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDERR_FILENO,TCSAFLUSH, &raw);
+
+}
 
 int main(){
+    enableRawMode();
 
+    char c;
+    while(read(STDIN_FILENO,&c,1)==1 && c!='q');
     return 0;
 }
