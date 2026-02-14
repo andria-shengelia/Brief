2e0784d89db22f7e89e27891914d905b5da13048
Andria Shengelia <shengeliaandria9@gmail.com>
2026-02-14 23:26:20 +0400

    Added Header file

--- main.c
+++ main.c
@@ -1,15 +1,5 @@
 /*** includes ***/
-
-#include <ctype.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <termios.h>
-#include <unistd.h>
-
+#include "header.h"
 /*** defines ***/
 
 #define KILO_VERSION "0.0.2"
